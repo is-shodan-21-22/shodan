01d406e7f14507419ab999ea6c06db3792984589
v1enna <19vienna20@gmail.com>
2021-06-14 15:53:20 +0200

    Fix blog list routines

---
+++ WebContent/Scripts/BlogListRoutines.js
@@ -0,0 +1,8 @@
+$(function() {
+	$(".blog-link").click(
+		function() {
+			window.history.pushState(null, null, "?blog=" + $(this).attr("data-blog-id"));
+			$("#app").load("View/Article.jsp");
+		}
+	);
+});
